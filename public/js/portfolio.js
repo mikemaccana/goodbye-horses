873f9630b49b4932fba95d6f7068d7d57cbf7faf
Mike MacCana <mike.maccana@gmail.com>
2012-02-18 22:12:51 +0000

    - Add neat shade of blue  - Make highlight snap back to currently selected nav item if the user leaves the nav area

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -2,12 +2,12 @@
 
 // Fetch and fill in the portfolio
 $.getJSON('../public/json/portfolio.json', function(portfolio_data) {
-					
+
+	// Fill in our template and add to document					
 	portfolio_data.works.forEach( function(work) {
 		work['slug_name'] = work.name.replace(/[\s-,']+/g, '').toLowerCase();
 	}) 
 	portfolio_data['year'] = new Date().getFullYear();
-
 	var portfolio = ich.portfolio_template(portfolio_data);
 	$('body').append(portfolio);
 			
@@ -47,11 +47,13 @@ $.getJSON('../public/json/portfolio.json', function(portfolio_data) {
 					
 });
 
-var nav_selector = 'nav > ul > li > a';
+var nav_selector = 'nav > #nav_container > ul > li > a';
 				
 $(document).on('click', nav_selector, function(event) {
 	console.log('nav bar clicked');
 	var $target = $(event.target);
+	$(nav_selector).not($target).removeClass('selected');
+	$(nav_item).addClass('selected');
 	var destination = $target.attr('href');
 	var title = $target.text();
 	$.get(destination, function() {
@@ -62,16 +64,29 @@ $(document).on('click', nav_selector, function(event) {
 });
 
 /* Slide nav highlight around as links are hovered */
-$(document).on('hover', nav_selector, function(event) {
-	console.log('nav bar hovered');
-	var offset = -45;
+var select_nav_item = function(nav_item) {
+	var offset = -5;
 	var extra_width = 10;
-	var nav_item = $(event.target);
+	
     var new_position = nav_item.position();
     var new_width = nav_item.outerWidth(true);
-    $('nav > #highlight').stop().animate({
+    $('nav > #nav_container > #highlight').stop().animate({
         'left': new_position.left + offset,
         'width': new_width + extra_width
-    });
+    }, 'fast');
+	
+}
+
+$(document).on('hover', nav_selector, function(event) {
+	console.log('nav bar item hovered');
+	var nav_item = $(event.target);
+    select_nav_item(nav_item);
 });
+
+// When the mouse leaves the ul, revert back to selected item
+$(document).on('hover', 'nav > #nav_container', function(){}, function(event){
+	console.log('nav bar un-hovered');
+	select_nav_item($('a.selected'));
+}) 
+
 	
