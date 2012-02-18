1516751f72fca602681422cb35e3cde42c6fbfbe
Mike MacCana <mike.maccana@gmail.com>
2012-02-18 14:29:23 +0000

    - Remove document ready - since we use client-side templating, there's no document to ready before we begin templating  - Fix location of event.preventDefault when nav links clicked

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,67 +1,61 @@
-$(function(){
-			
-	// Fetch and fill in the portfolio
-	$.getJSON('../public/json/portfolio.json', function(portfolio_data) {
-					
-		portfolio_data.works.forEach( function(work) {
-			work['slug_name'] = work.name.replace(/[\s-,']+/g, '').toLowerCase();
-		}) 
-		portfolio_data['year'] = new Date().getFullYear();
+// No waiting for document ready, because we don't have a document yet!
 
-		var portfolio = ich.portfolio_template(portfolio_data);
-		$('body').append(portfolio);
-			
-		var sort_works = function(sort_key) {
-			// Return a list of the work with the tag/sort_key mentioned
-			var sorted_works = [];
-			portfolio_data.works.forEach(function(work) {
-				if ( work.tags.indexOf(sort_key)+1 ) {
-					sorted_works.push(work);
-				}
-			})
-			return sorted_works					
-		};
-		
-		$(document).on('click', '#works > li > ul > li.tag', function(event){	
-			// Sort works when tags clicked 
-			console.log('Tag clicked')
-			var sort_key = $(event.target).text();
-				
-			sorted_works = {
-				works: sort_works(sort_key)
-			}
-				
-			var sorted_portfolio = ich.sorted_works_template(sorted_works);
-			$('#sorted_works').replaceWith(sorted_portfolio);
-				
-			$('#works').quicksand( $('#sorted_works li.work') );
-		});
-		
-		// Mouseover animation for tags 			
-		var tags = '#works > li > ul > li.tag';
-		$(document).on("mouseenter", tags, function() {
-			$(event.target).animate({ backgroundColor: "#222" }, 'slow');
-		}).on("mouseleave", tags, function() {
-			$(event.target).animate({ backgroundColor: "#777" }, 'slow');
-		});
+// Fetch and fill in the portfolio
+$.getJSON('../public/json/portfolio.json', function(portfolio_data) {
 					
-	})
-				
+	portfolio_data.works.forEach( function(work) {
+		work['slug_name'] = work.name.replace(/[\s-,']+/g, '').toLowerCase();
+	}) 
+	portfolio_data['year'] = new Date().getFullYear();
+
+	var portfolio = ich.portfolio_template(portfolio_data);
+	$('body').append(portfolio);
 			
-				
-				
-})
+	var sort_works = function(sort_key) {
+		// Return a list of the work with the tag/sort_key mentioned
+		var sorted_works = [];
+		portfolio_data.works.forEach(function(work) {
+			if ( work.tags.indexOf(sort_key)+1 ) {
+				sorted_works.push(work);
+			}
+		})
+		return sorted_works					
+	};
 		
-console.log('hello');
+	$(document).on('click', '#works > li > ul > li.tag', function(event){	
+		// Sort works when tags clicked 
+		console.log('Tag clicked')
+		var sort_key = $(event.target).text();
+				
+		sorted_works = {
+			works: sort_works(sort_key)
+		}
+				
+		var sorted_portfolio = ich.sorted_works_template(sorted_works);
+		$('#sorted_works').replaceWith(sorted_portfolio);
+				
+		$('#works').quicksand( $('#sorted_works li.work') );
+	});
+		
+	// Mouseover animation for tags 			
+	var tags = '#works > li > ul > li.tag';
+	$(document).on("mouseenter", tags, function() {
+		$(event.target).animate({ backgroundColor: "#222" }, 'slow');
+	}).on("mouseleave", tags, function() {
+		$(event.target).animate({ backgroundColor: "#777" }, 'slow');
+	});
+					
+});
+				
 $(document).on('click', 'nav > ul > li > a', function(event) {
+	console.log('nav bar clicked');
 	var $target = $(event.target);
 	var destination = $target.attr('href');
 	var title = $target.text();
 	$.get(destination, function() {
 		history.pushState({}, title, destination);	
-		$('section').fadeOut();
-		event.preventDefault();	
-	})
+		$('section').fadeOut();	
+	});
+	event.preventDefault();	
 });
-			
-console.log('set up handler');
+	
