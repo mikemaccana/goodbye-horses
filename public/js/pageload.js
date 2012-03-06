9b27f474e5f2d574b22444a1accf5142ec519363
Mike MacCana <mike.maccana@gmail.com>
2012-03-06 11:00:12 +0000

    Further work in progress. Going to try scrapping everything and moving to backbone.

--- public/js/pageload.js
+++ public/js/pageload.js
@@ -1,6 +1,6 @@
-var edition_loaded = false;
-var edition_data = {};
-var templates_loaded = [];
+var edition_data = {}; // A summary of what templates and content are required to load a URL
+var content = {}; // Template and content data, with name on as key, content as value
+var templates_loaded = []; // Templates loaded into mustache
 var debug = true;
 
 // Log if debugging enabled
@@ -34,7 +34,7 @@ function load_edition() {
 }
 
 // Return a list of all parent URLs for the given URL
-function get_url_parents(path) {
+function get_parent_urls(path) {
 	var templates = []
 	var path_bits = path.split('/').splice(1)
 
@@ -65,62 +65,79 @@ function encode_url(base, parts_to_encode) {
 // Load whatever page is at path into the window
 function load_page(path) {
 	if ( ! edition_data.hasOwnProperty(path) ) {
-		debuglog('Page doesnt exist - loading 404 oage')
+		debuglog('Page doesnt exist - loading 404 page')
 		path = 'notfound'
 	}
 
-	templates_and_data_needed = []
-
 	// Get a list of all templates needed for our parent frames (eg, /foo/bar depends on / and /foo)
-	parents = get_url_parents(path)
-	parents.forEach( function(parent_url) {
-		this_url = edition_data[parent_url]
+	var templates_and_data_needed = []
+	var parent_urls = get_parent_urls(path)
+	console.log('parent_urls:')
+	console.log(parent_urls)
+	parent_urls.forEach( function(ancestor_url) {
+		this_url = edition_data[ancestor_url]
 		templates_and_data_needed.push(['/'+this_url['frame_template'], '/'+this_url['frame_contents']])
-	})	
-	// And the template for the current page too
+	})		
 	templates_and_data_needed.push(['/'+edition_data[path]['template'], '/'+edition_data[path]['contents'] ])
+
 
 	// Request the required templates from the server
 	console.log('templates_and_data_needed templates_and_data_needed templates_and_data_needed templates_and_data_needed')
 	console.log(templates_and_data_needed)
 	
 	thing_we_need_url = encode_url('/templates_and_data', templates_and_data_needed)
-	$.get( thing_we_need_url, {}, function(response_data) {
+	$.get( thing_we_need_url, {}, function(templates_and_data) {
 		console.log('Recieved templates and data')
-		console.log(response_data);
-		load_templates_and_contents(templates_and_data_needed, response_data)
+		console.log(templates_and_data);
+		load_templates_and_contents(parent_urls, templates_and_data_needed, templates_and_data)
 	})
 }
 
-// Stuff the template with the contents
-function load_templates_and_contents(templates_and_data_needed, response_data) {
-	// Work through current url
-	parents = get_url_parents(location.pathname)
-	console.log(parents)
-	parents.forEach( function(parent) {
-		template_name = edition_data[parent_url]['template']
-		template_data = edition_data[parent_url]['template']
-	})
-	
-	/*templates_and_data_needed.forEach( function(template_and_content_pair) {
-		console.log(template_and_content_pair)
-		template = template_and_content_pair[0]
-		content = template_and_content_pair[1]
+// Get the template and contents from the server, add them to our global list, then load them
+function load_templates_and_contents(parent_urls, templates_and_data_needed, templates_and_data) {
+	// Update our global list of templates and content, and load any new templates into ICHmustache
+	for ( url in templates_and_data ) {
+		if ( array_has_item(url, 'content') ) {
+			content[url] = templates_and_data[url];
+		} else {
+			add_template_if_necessary(url, templates_and_data[url])
+		}	
+	}
+	// Could just trigger an event
+	put_html_on_page(parent_urls)
+}
+
+// Add each ancestors template and contents until we finally add the real page.
+function put_html_on_page(parent_urls) {
+	parent_urls.forEach( function(url) {
+		console.log('______________________')
+		console.log('url:')	
+		console.log(url)
+		console.log('and according to edition data we need the following template:')
 		
-	})*/
-	
-	/*$.get('/mustache/'+template+'.mustache', function(template_string) {
-		$.get('/json/'+contents+'.json', function(contents) {
-			add_template_if_necessary(template, template_string);
-			debuglog('contents are:')
-			debuglog(contents)
-			var html = ich[template](contents);
-			debuglog(html)
-		})
-	})*/
+		var template_needed = edition_data[url]['frame_template']
+		console.log(template_needed)
+		console.log(ich[template_needed])
+		
+		
+		console.log('and contents named...')
+		var contents_needed_name = edition_data[url]['frame_contents']
+		console.log(contents_needed_name)
+		// undefined :^()	
+		console.log('with the following JSON content...')
+		console.log(content['/'+contents_needed_name])
+		
+		debuglog('edition_data')
+		debuglog(edition_data)
+		
+		// there is no content[works_frame_content]
+		debuglog('content')
+		debuglog(content)
+
+		//console.log('html for that would be:')
+		//var html = ich[template_needed](contents_needed_mustache_string);	
+	})
 }
-
-
 
 // Load nav, and default page the first time you get this file
 if ( Object.keys(edition_data).length === 0 ) {
